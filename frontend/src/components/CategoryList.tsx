a8d13f77a1d02c27a9712e0ff4bccd6a852007d1
Johanna <johanna.kveton@gmail.com>
2025-07-15 21:07:22 +0200

    refactor category and product list

--- frontend/src/components/CategoryList.tsx
+++ frontend/src/components/CategoryList.tsx
@@ -1,62 +1,32 @@
-import { CategoryType, ProductType } from "../types/types";
-import React, { useState } from "react";
+import { CategoryType } from "../types/types";
+import React, { Dispatch, SetStateAction } from "react";
 import "./CategoryList.css";
-import ProductList from "./ProductList";
-import Featured from "./Featured";
-import useFetchProducts from "../contexts/useFetchProducts";
 
 type CategoryListProps = {
   categoryList: CategoryType[];
+  setSelectedCategory: Dispatch<SetStateAction<CategoryType | null>>;
 };
 
-const CategoryList = ({ categoryList }: CategoryListProps) => {
-  const [productsByCategory, setProductsByCategory] = useState<ProductType[]>(
-    []
-  );
-  const [selectedCategory, setSelectedCategory] = useState<CategoryType | null>(
-    null
-  );
-
-  const { productList } = useFetchProducts();
-  const [error, setError] = useState("");
-
-  const handleSelectCategory = (category: CategoryType) => {
-    setSelectedCategory(category);
-    setProductsByCategory(
-      productList.filter((prod) => prod.category === category._id)
-    );
-  };
-
+const CategoryList = ({
+  categoryList,
+  setSelectedCategory,
+}: CategoryListProps) => {
   return (
-    <>
-      <div id="category-list-wrap">
-        {categoryList.length ? (
-          categoryList.map((category) => (
-            <button
-              id="category-button"
-              key={category._id}
-              onClick={() => handleSelectCategory(category)}
-            >
-              {category.name.toUpperCase()}
-            </button>
-          ))
-        ) : (
-          <h2>An error occured when loading the categories.</h2>
-        )}
-      </div>
-      {error ? (
-        <p>{error}</p>
-      ) : selectedCategory && productsByCategory.length ? (
-        <ProductList
-          productList={productsByCategory}
-          categoryName={selectedCategory.name}
-        ></ProductList>
-      ) : selectedCategory && !productsByCategory.length ? (
-        <p>No products for this category.</p>
+    <div id="category-list-wrap">
+      {categoryList.length ? (
+        categoryList.map((category) => (
+          <button
+            id="category-button"
+            key={category._id}
+            onClick={() => setSelectedCategory(category)}
+          >
+            {category.name.toUpperCase()}
+          </button>
+        ))
       ) : (
-        <Featured></Featured>
+        <h2>An error occured when loading the categories.</h2>
       )}
-    </>
+    </div>
   );
 };
 
--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -1,15 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { CategoryType } from "../types/types";
+import { CategoryType, ProductType } from "../types/types";
 import { getCategories } from "../services/category-service";
 import CategoryList from "../components/CategoryList";
 import Hero from "../components/Hero";
+import { getProducts } from "../services/product-service";
+import ProductList from "../components/ProductList";
+import Featured from "../components/Featured";
 
 const Home = () => {
   const [categoryList, setCategoryList] = useState<CategoryType[]>([]);
-  const [error, setError] = useState("");
+  const [productList, setProductList] = useState<ProductType[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<CategoryType | null>(
+    null
+  );
+  const [categoryError, setCategoryError] = useState("");
+  const [productError, setProductError] = useState("");
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
-    const fetchAndSet = async () => {
+    const fetchAndSetCategories = async () => {
       try {
         const allCategories = await getCategories();
         setCategoryList(allCategories);
@@ -18,19 +27,59 @@ const Home = () => {
           err instanceof Error
             ? err.message
             : "An unknown error occurred. Sorry!";
-        setError(errorMessage);
+        setCategoryError(errorMessage);
       }
     };
-    fetchAndSet();
+    fetchAndSetCategories();
   }, []);
 
+  useEffect(() => {
+    const fetchAndSetProducts = async () => {
+      try {
+        const allProducts = await getProducts();
+        setProductList(allProducts);
+        if (selectedCategory)
+          setProductList((prev) =>
+            prev.filter((prod) => prod.category === selectedCategory._id)
+          );
+      } catch (err) {
+        const errorMessage =
+          err instanceof Error
+            ? err.message
+            : "An unknown error occurred. Sorry!";
+        setProductError(errorMessage);
+      }
+    };
+    fetchAndSetProducts();
+  }, [selectedCategory, query]);
+
+  const showProducts = selectedCategory;
   return (
     <>
-      <Hero></Hero>
-      {error ? (
-        <p>{error}</p>
+      <Hero />
+      {categoryError ? (
+        <p>{categoryError}</p>
       ) : (
-        <CategoryList categoryList={categoryList}></CategoryList>
+        <CategoryList
+          categoryList={categoryList}
+          setSelectedCategory={setSelectedCategory}
+        ></CategoryList>
+      )}
+      {productError ? (
+        <p>{productError}</p>
+      ) : (
+        <>
+          {showProducts && productList.length !== 0 ? (
+            <ProductList
+              productList={productList}
+              categoryName={selectedCategory.name}
+            ></ProductList>
+          ) : !productList.length ? (
+            <p>No products for this category.</p>
+          ) : (
+            <Featured></Featured>
+          )}
+        </>
       )}
     </>
   );
